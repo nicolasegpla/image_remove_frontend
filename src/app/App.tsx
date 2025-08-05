d444a11e39f13c8674c4b5fa168e4009317bb7fc
nicolas <nicolasgpla@gmail.com>
2025-08-04 22:46:21 -0500

    feat:  hydrate token from sessionStorage on page reload

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,21 +4,8 @@ import { useTokenStore } from '@/store/zustand/useTokenstore';
 
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
-import { useEffect } from 'react';
 
 function AppContent() {
-    const setToken = useTokenStore((state) => state.setToken);
-
-    useEffect(() => {
-        const session = sessionStorage.getItem('session');
-        if (session) {
-            const parsed = JSON.parse(session);
-            if (parsed?.token) {
-                setToken(parsed.token);
-            }
-        }
-    }, [setToken]);
-
     const { isAuthenticated } = useTokenStore();
     const { AppRoutes } = useRoutesProvider({ token: isAuthenticated });
     return (
